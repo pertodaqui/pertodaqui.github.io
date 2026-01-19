e8bca09f3b577bf266f3530cea00ccb120c9b6ee
Paulo Roberto <paulo.pinda@gmail.com>
2026-01-18 22:59:56 -0300

    added itens.

--- pages/_app.js
+++ pages/_app.js
@@ -5,16 +5,26 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Script
-        src="https://www.googletagmanager.com/gtag/js?id=G-6HQDMXR4BL"
+        src="https://www.googletagmanager.com/gtag/js?id=G-4PK6JRYRHV"
         strategy="afterInteractive"
       />
       <Script id="ga-gtag" strategy="afterInteractive">
         {`window.dataLayer = window.dataLayer || [];
 function gtag(){window.dataLayer.push(arguments);}
 gtag('js', new Date());
-gtag('config', 'G-6HQDMXR4BL');`}
+gtag('config', 'G-4PK6JRYRHV');`}
       </Script>
       <Component {...pageProps} />
     </>
   );
 }
+
+// <!-- Google tag (gtag.js) -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=G-4PK6JRYRHV"></script>
+// <script>
+//   window.dataLayer = window.dataLayer || [];
+//   function gtag(){dataLayer.push(arguments);}
+//   gtag('js', new Date());
+
+//   gtag('config', 'G-4PK6JRYRHV');
+// </script>
