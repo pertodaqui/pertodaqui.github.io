e563952f4abf133d12706e78f918a0c18c8640b4
Paulo Roberto <paulo.pinda@gmail.com>
2026-01-18 22:35:52 -0300

    added itens

--- data/nature.js
+++ data/nature.js
@@ -1,5 +1,5 @@
 const nature = [
-    {
+  {
     "id": "2",
     "title": "Bosque da Princesa",
     "meta": "Um refúgio histórico e arborizado às margens do Rio Paraíba do Sul, com árvores centenárias e ambiente relaxante.",
@@ -79,7 +79,34 @@ const nature = [
     "image": "https://s0.wklcdn.com/image_10/312416/20490480/12823803Master.jpg",
     "lat": -22.767625054619245, 
     "lng": -45.41616412281976
-  }
+  },
+  {
+    "id": "cachoeira-marsilac",
+    "title": "Cachoeira de Marsilac",
+    "meta": "Localizada no rio Capivari, o último rio limpo da capital. Oferece infraestrutura de aventura e prainha de água doce.",
+    "location": "Engenheiro Marsilac, São Paulo - SP, Brasil",
+    "image": "https://lh3.googleusercontent.com/gps-cs-s/AG0ilSwEpW6mQ1-HhNPzFQlQlkOVrLwp_Pn1zOIqufc8vORvrHjfNNj4JR-CgJ7y9a8BGUV1kxPRVEFGO208luy-S_cUjSC7EoverBUyIKp8OHUmI_gXOqR22dCdXtdaEn86Cxf_KTnF=w270-h312-n-k-no",
+    "lat": -23.94112705650625, 
+    "lng": -46.6910825067857
+  },
+  {
+    "id": "cachoeira-do-sagui",
+    "title": "Cachoeira do Sagui",
+    "meta": "Situada na Fazenda Maravilha, possui uma trilha leve de 1,2km em meio à Mata Atlântica preservada.",
+    "location": "Estrada da Ponte Alta, São Paulo - SP, Brasil",
+    "image": "https://lh3.googleusercontent.com/gps-cs-s/AG0ilSwrAMPIuwMfkH8A8o_8C2z5MXNLGSe8GBKnM6nA665fW8YQMfbYiVllZDsUVh3ZT44xd11_8Z5K8p5pYGUnK32pvSiXbBL-OQv66j1PJJh3Gkg-mEycyay3mgio32dPuXxGvFQvkg=w425-h240-k-no",
+    "lat": -23.94760177357235, 
+    "lng": -46.72770990446995
+  },
+  {
+    "id": "cachoeira-do-engordador",
+    "title": "Cachoeira do Engordador",
+    "meta": "Fica no Parque Estadual da Cantareira. A trilha é circular e inclui tanques de decantação históricos.",
+    "location": "Serra da Cantareira, São Paulo - SP, Brasil",
+    "image": "https://lh3.googleusercontent.com/gps-cs-s/AG0ilSw0cD2M7J54s7GZG6xvfSR7rKh1g_-MG17qd9ClbW0memmqyjb8mKwa2fhtx82PhTGypXdz4DEID-N4r6TYJrwbq3xHSp8g74Rf9PaWOLNikMu2LGCrhDZc3O3Z7yaJk5M7GuAFunRiG6co=w408-h306-k-no",
+    "lat": -23.389107248735844, 
+    "lng": -46.5866114581063
+  },
 ];
 
 export default nature;
