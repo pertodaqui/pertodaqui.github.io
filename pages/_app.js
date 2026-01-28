ad0bf38940ad8c7e1b77658c81b47eb281355e80
Paulo Roberto <paulo.pinda@gmail.com>
2026-01-27 22:22:40 -0300

    added pwa support.

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,24 @@
+import Head from "next/head";
 import Script from "next/script";
+import { useEffect } from "react";
 import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    if ("serviceWorker" in navigator) {
+      window.addEventListener("load", () => {
+        navigator.serviceWorker.register("/service-worker.js");
+      });
+    }
+  }, []);
+
   return (
     <>
+      <Head>
+        <meta name="theme-color" content="#0e45a9" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/pwa/icon-192.png" />
+      </Head>
       <Script
         src="https://www.googletagmanager.com/gtag/js?id=G-4PK6JRYRHV"
         strategy="afterInteractive"
