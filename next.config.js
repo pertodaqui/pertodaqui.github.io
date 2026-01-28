c2fa247289bd5e0d3df1a40d043d30419970c0c8
Paulo Roberto <paulo.pinda@gmail.com>
2026-01-28 19:10:49 -0300

    added obfuscator.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,37 @@
 /** @type {import('next').NextConfig} */
+const JavaScriptObfuscator = require("webpack-obfuscator");
+
 const nextConfig = {
   output: "export",
   trailingSlash: true,
   images: {
     unoptimized: true
+  },
+  webpack: (config, { dev, isServer }) => {
+    if (!dev && !isServer) {
+      config.plugins.push(
+        new JavaScriptObfuscator(
+          {
+            compact: true,
+            controlFlowFlattening: true,
+            controlFlowFlatteningThreshold: 0.75,
+            deadCodeInjection: true,
+            deadCodeInjectionThreshold: 0.4,
+            disableConsoleOutput: true,
+            identifierNamesGenerator: "hexadecimal",
+            rotateStringArray: true,
+            selfDefending: true,
+            stringArray: true,
+            stringArrayEncoding: ["base64"],
+            stringArrayThreshold: 0.75,
+            splitStrings: true,
+            splitStringsChunkLength: 8
+          },
+          []
+        )
+      );
+    }
+    return config;
   }
 };
 
